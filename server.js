bec4043ed83e223fca72632d1e9c3a3197b682ae
ronakh-thaikkat <ronakhprakash@gmail.com>
2016-09-22 08:28:36 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-app.get('/ui/main.js ', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
