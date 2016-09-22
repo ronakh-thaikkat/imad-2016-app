8dc094a7bb34b89e58530b03569b4de7476a074d
ronakh-thaikkat <ronakhprakash@gmail.com>
2016-09-22 08:09:21 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+//make the image move
+
+var pictureElement = document.getElementById('picture');
+
+
+var interval = setInterval(moveRight , 50);
+var marginLength = 0;
+var moveRight = function() {
+    var marginLength = marginLength + 10;
+    picture.style.marginLength = marginLength + "px";
+};
+
