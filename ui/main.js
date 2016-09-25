f2815baaabf9c94c3be22259e99aa2bd1295b06c
ronakh-thaikkat <ronakhprakash@gmail.com>
2016-09-25 10:29:57 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,10 +10,11 @@ myElement.style.backgroundColor = "#D93600";  */
 
 var request = new XMLHttpRequest();
 request.onreadychangestate = function(){
-    if(request.readyState === XMLHttpRequest.DONE && request.status === 200){
-      var counter = request.responseText;
-      var span = document.getELementById('count');
-      span.innerHTML = counter.toString();
+    if(request.readyState === XMLHttpRequest.DONE {
+      if(request.status === 200){
+       var counter = request.responseText;
+       var span = document.getElementById('count');
+       span.innerHTML = counter.toString();
     }
 
 
